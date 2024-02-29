2db57c33bba552f8e6efddf25f99161abbddb5ee
Suraj Singh Mehra <surajsinghmehra04@gmail.com>
2024-02-29 20:06:03 +0530

    adding to render

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react'
 
 export default function About() {
-    return (
-        <h1 className="text-3xl font-bold underline">
-      About
-    </h1>
-    )
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About CozeyNest</h1>
+      <p className='mb-4 text-slate-700'>The CozeyNest project is an innovative platform designed to streamline the process of finding student accommodations. </p>
+      <p className='mb-4 text-slate-700'>
+      The objective of CozeyNest is to offer a centralized hub where students can effortlessly discover, assess, and communicate with accommodation options that align with their preferences. The platform incorporates features such as real-time search, detailed accommodation listings, secure communication channels, and a robust feedback system.
+      </p>
+      <p className='mb-4 text-slate-700'>CozeyNest stands as a transformative force in the realm of student accommodations, offering a technologically advanced and user-friendly platform that reshapes the student living experience. The project's success lies in its commitment to transparency, community building, and continuous adaptation to emerging trends and technologies.</p>
+    </div>
+  )
 }
