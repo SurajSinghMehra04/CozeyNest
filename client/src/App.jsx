22a557a067645948e130cf270a52d1ef82347da1
Suraj Singh Mehra <surajsinghmehra04@gmail.com>
2024-03-02 14:36:34 +0530

    profile bug solve

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
-import Profile from './pages/Profile';
+import Profile from './pages/profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
